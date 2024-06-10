7b54df00e2f8e8ada462c3d3ccd92ae42afc6e88
spdx-github <distantsignalis@gmail.com>
2024-06-10 16:49:02 +0200

    finally got flip working, and added aux cargo slot

--- IndustriesOfEnceladusRevamp/ships/modules/CargoAuxBase.gd
+++ IndustriesOfEnceladusRevamp/ships/modules/CargoAuxBase.gd
@@ -1,5 +1,4 @@
-#extends CollisionPolygon2D
-extends RigidBody2D
+extends CollisionPolygon2D
 
 export  var repairReplacementPrice = 1000
 export  var repairReplacementTime = 1
@@ -7,21 +6,24 @@ export  var repairFixPrice = 1000
 export  var repairFixTime = 1
 var equipment = true
 
-export var systemMass = 1000
+export var mass = 1000
 export  var systemName = "SYSTEM_CARGO_AUX"
 export  var slot = "cargo.aux"
 
 export (float, 0, 25000, 1000) var internalStorage = 0.0
 export  var registerExternal = false
 
-#export var mirrorCollider = false
-#export (Vector2) var mirrorCentreOffset = Vector2(0,0)
+export var mirrorCollider = false
+export (Vector2) var mirrorCentreOffset = Vector2(0,0)
 
 var ship
 
 func _ready():
 	ship = getShip()
 	if ship.getConfig(slot) != systemName:
+		Tool.remove(self)
+	else: if not (self in ship.get_children()):
+		ship.add_child(self.duplicate())
 		Tool.remove(self)
 	else :
 		visible = true
@@ -32,21 +34,43 @@ func _ready():
 			ship.processedCargoCapacity += internalStorage * 6
 		else:
 			ship.processedCargoCapacity += internalStorage
+		
+		extend(ship)
 			
-#		if mirrorCollider:
-#			var copy = CollisionPolygon2D.new()
-#			var copyTf = self.transform
-#			var copyScale = self.scale
-#
-#			copy.transform = - (copyTf.x) + copyTf.y + mirrorCentreOffset
-#			copy.scale = -(copyScale.x) + copyScale.y
-#
-#			copy.set_script(load("res://ships/MaterialReadout.gd"))
-#
-#			for child in copy.get_children():
-#				copy.remove_child(child)
-#
-#			ship.call_deferred("add_child", copy)
+		# flip the collider if it's requested
+		if mirrorCollider:
+			# get the position and scale of the existing collider
+			var selfPos = self.get_position()
+			var selfScale = self.scale
+			# instantiate a new collider
+			var copy = CollisionPolygon2D.new()
+			
+			# copy over properties
+			copy.visible = true
+			copy.z_index = self.z_index
+			copy.set_polygon(self.polygon)
+			copy.set_build_mode(self.build_mode)
+			copy.set_disabled(false)
+			copy.set_one_way_collision(self.one_way_collision)
+			copy.set_one_way_collision_margin(self.one_way_collision_margin)
+			
+			# flip the x coordinate and adjust for the centre offset
+			copy.set_position(Vector2(-(selfPos[0]), selfPos[1])
+				+ mirrorCentreOffset)
+			# set the scale to the equivalent of self
+			copy.set_scale(Vector2(-(selfScale[0]), selfScale[1]))
+			
+			# clear the script, just to be sure
+			copy.set_script(null)
+			# set a property for the cargo scanner? i'm not sure
+			copy.equipment = true
+			# add the collider to the ship
+			ship.add_child(copy)
+			
+			#print("Made copy %s with scale %s at %s, parent is %s" % [copy.to_string(), String(copy.scale), String(copy.position), copy.get_parent().to_string()])
+
+func extend(ship):
+	return true
 
 func getShip():
 	var c = self
---
+++ IndustriesOfEnceladusRevamp/ships/modules/CargoAuxPreProc.gd
@@ -0,0 +1,117 @@
+extends "res://IndustriesOfEnceladusRevamp/ships/modules/CargoAuxBase.gd"
+
+# ok this is very stupid
+# _physics_process can't be explicitly overriden in 3.5.3
+# goddamnit. i have to cpy the entire fucking thing
+# fuck! fuck! fuck!
+
+export  var ventTime = 0.5
+export  var massDamageScale = 20.0
+export  var kgps = 100
+export  var powerDrawPerKg = 100
+export (float, 0, 1, 0.05) var mineralEfficiency = 0.0
+export (float, 0, 1, 0.05) var remassEfficiency = 0.5
+
+export  var enabled = true
+
+var processing = []
+var power = 0
+
+func getStatus():
+	return 100
+func getPower():
+	return clamp(power, 0, 1)
+
+func extend(ship):
+	# and this part which boosts MPU efficiency
+	for node in ship.get_children():
+		if node.slot == "cargo.equipment":
+			var nodeMinEff = node.get("mineralEfficiency")
+			var newMinEff = clamp(nodeMinEff + (nodeMinEff * mineralEfficiency),
+				0, 0.95)
+			# clamped to 0.95 since it'll violate the laws of physics otherwise
+			# maybe 1? i can change it later i guess
+			ship.set("mineralEfficiency", newMinEff)
+			
+onready var ventRemass = $VentRemass
+onready var processingA = $Processing
+onready var proStart = $ProStart
+onready var proStop = $ProStop
+	
+var ventingMineral = 0.0
+
+func _physics_process(delta):
+	ventingMineral = max(0, ventingMineral - delta)
+	power = 0
+
+	var venting = false
+	var isproc = false
+	
+	if enabled:
+		for p in processing:
+			if Tool.claim(p):
+				if "fillerContent" in p:
+					if p.fillerContent > 0.01:
+						var fillerMass = p.fillerContent * p.mass
+						var mineralMass = p.mineralContent * p.mass
+						var procDelta = min(fillerMass, delta * kgps / 1000)
+						var requiredPower = procDelta * powerDrawPerKg * 1000
+						var gotPower = ship.drawEnergy(requiredPower)
+						if gotPower / requiredPower > 0.9:
+							# for some reason it doesn't work if this isn't in place
+							var nm = max(mineralMass, p.mass - procDelta)
+							
+							if nm > 0:
+								p.mass = nm
+								p.mineralContent = mineralMass / nm
+							
+							p.fillerContent = 1 - p.mineralContent
+							# stuff from the original imp
+							var newMass = max(mineralMass, p.mass - procDelta)
+							var massChange = p.mass - newMass
+							var shipRemass = ship.reactiveMass
+							var procRemass = massChange * 1000 * remassEfficiency
+							var newRemass = clamp(shipRemass + procRemass, 0, ship.reactiveMassMax)
+							ship.reactiveMass = newRemass
+							isproc = true
+							power += 1.0
+							venting = (newRemass - shipRemass < procRemass / 2)
+							ship.cargoMass = max(ship.cargoMass - massChange * 1000, 0)
+				else :
+					if p.has_method("getScan") and p.getScan() == "H2O" and p.has_method("applyEnergyDamage"):
+						var mad = max(1, p.mass / massDamageScale)
+						var proc = min(p.mass, delta * kgps / 1000) * mad
+						var requiredPower = proc * powerDrawPerKg * 1000 * delta * 60
+						var gotPower = ship.drawEnergy(requiredPower)
+						var prm = ship.reactiveMass
+						if gotPower / requiredPower > 0.9:
+							p.applyEnergyDamage(gotPower, p.global_position, delta)
+							var st = remassEfficiency * proc * 1000
+							var nrm = clamp(prm + st, 0, ship.reactiveMassMax)
+							ship.reactiveMass = nrm
+							isproc = true
+							power += 1.0
+							venting = (nrm - prm < st / 2)
+				Tool.release(p)
+							
+	ventRemass.emitting = venting
+	
+	if ship.isPlayerControlled():
+		if isproc:
+			if not processingA.playing:
+				processingA.play()
+				proStart.play()
+		else :
+			if processingA.playing:
+				processingA.stop()
+				proStop.play()
+
+
+									
+func _on_ProcessingArea_body_entered(body):
+	
+	processing.append(body)
+
+func _on_ProcessingArea_body_exited(body):
+	
+	processing.remove(processing.find(body))
